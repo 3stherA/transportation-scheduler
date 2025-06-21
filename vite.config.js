ee0dee12efabb27e1a33c3cb907c0ae18efee28c
Esther Adeosun <estheradeosun@Esthers-MacBook-Air-4.local>
2025-06-21 17:17:16 -0400

    updated repo name in vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/transportation-scheduler/',
+  base: '/transportation-scheduler-frontend/',
   plugins: [react()]
 })
